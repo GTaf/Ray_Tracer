222fb4037ec92ecaea503110e1923bde0c9895fb
BenGallou <gallouedec.benoit@gmail.com>
2017-05-24 01:22:25 +0200

    Update phongReflectionModel.cpp

--- phongReflectionModel.cpp
+++ phongReflectionModel.cpp
@@ -1,19 +1,20 @@
+#include <cmath>
 #include "phongReflectionModel.h"
 
-Color phongColor(const Camera& cam,const Scene& scene, const std::vector<Light>& lights, const Sphere& sphere,const Vector& pos){
+Color phongColor(const Camera& cam,const Scene& scene,const std::vector<Light>& lights, const Sphere& sphere,const Vector& pos){
   Color c;
   for(int i=0;i<2;i++){
-    Material *m=&(sphere.getMaterial());
-    double ambiantLighting = m->getAmbient()*scene.getAmbiantLighting();
+    Material m=sphere.getMaterial();
+    double ambiantLighting = m.getAmbient()*scene.getAmbiantLighting();
     double diffuseLighting=0.;
     double specularLighting=0.;
-    for(std::vector<Light>::iterator it=lights.begin(); it!=lights.end(); ++it){
+    for(std::vector<Light>::const_iterator it=lights.begin(); it!=lights.end(); ++it){
       Vector L=(pos-it->getPoint()).normalize();
       Vector N=sphere.getNormal(pos);
       Vector R=2*(L*N)*N-L;
       Vector V=(cam.getEye()-pos).normalize();
-      diffuseLighting+=m->getDiffuse()*(L*N)*it->getDiffuse();
-      specularLighting+=m->getSpecular()*pow(R*V,m->getAlpha())*it->getSpecular();
+      diffuseLighting+=m.getDiffuse()*(L*N)*it->getDiffuse();
+      specularLighting+=m.getSpecular()*pow(R*V,m.getAlpha())*it->getSpecular();
     }
     c.setValue(c.getValue(i)+ambiantLighting+diffuseLighting+specularLighting,i);
   }
