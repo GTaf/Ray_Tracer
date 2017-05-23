d52e29fad7dde2a04de2694773d8e6386585102f
BenGallou <gallouedec.benoit@gmail.com>
2017-05-23 22:54:07 +0200

    Update PhongReflectionModel.h

--- PhongReflectionModel.h
+++ PhongReflectionModel.h
@@ -3,4 +3,13 @@
 #include <stdlib>
 #include "basics.h"
 
-Color phongColor(const& Camera cam,const& Scene scene, const& std::vector<Light> lights, const& Sphere sphere,const& Vector pos);
+/**
+* Cette fonction calcule la couleur perçue par la caméra d'un point donné sur une sphère.
+*   @param cam    la Camera
+*   @param scene  la scene
+*   @param lights l'ensemble des lumières de la scène
+*   @param sphere la sphere sur laquelle se trouve le point considéré
+*   @param pos    la position du point
+*   @return       un objet de classe Color représentant la couleur perçue
+*/
+Color phongColor(const Camera& cam,const Scene& scene, const std::vector<Light>& lights, const Sphere& sphere,const Vector& pos);
