6162444b76f5d85cc3b858e6c7229e30b4885a0b
BenGallou <gallouedec.benoit@gmail.com>
2017-05-29 19:02:30 +0200

    Résolution du problème d'intersection. Si le point de départ du rayon est dans la sphère, aucune intersection n'est détectée

--- main.cpp
+++ main.cpp
@@ -33,14 +33,11 @@ Color computeColor(Camera ca, Scene s, Ray origin, int index);
 void tracer(Camera ca, Scene s);
 
 int main(int argc, char** argv) {
-
     Sphere s = Sphere(250, Vector(500,0,0), Color(255,50,0), Material(0.5,0.5,0.5,20.));
     Ray r = Ray(Vector(1,0,0), Vector(0,5,0));
     double dist;
     bool b = ray_sphere_intersect(r,s,&dist);
-    //cout << b << endl;
-
-    Camera ca = Camera(Vector(0,0,0), Vector(100,0,0), Vector(0,1,0), 1000, 1000);
+    //cout << b << endl;  Camera ca = Camera(Vector(0,0,0), Vector(100,0,0), Vector(0,1,0), 1000, 1000);
     Scene sc = Scene(0);
     sc.setAmbiantLighting(0.001);
     sc.addSphere(s);
@@ -80,16 +77,18 @@ bool ray_sphere_intersect(Ray r, Sphere s, double *dist){//le rayon part du poin
     return d >= 0;
 }
 
-/*A modifier !!!!!!!!!! Intersection que dans un seul sens*/
+/*Dans cette version, si on est dans une boule elle est invisible (Pas d'intersection détectée)
+* Le problème des intersections avec des boules derrière le rayon est réglé.
+*/
 bool ray_sphere_intersect2(Ray r, Sphere s,Vector& pos){//le rayon part du point de vision
   Vector m=s.getCenter()-r.getPoint();
-    Vector a=(m*r.getVector().normalize())*r.getVector().normalize();
+    Vector a=abs((m*r.getVector().normalize()))*r.getVector().normalize();
     Vector b=m-a;
     if(b.norm()-s.getRadius()>0)return false;
     double d=sqrt(pow(s.getRadius(),2)-pow(b.norm(),2));
     pos=(a.norm()-d)*r.getVector().normalize()+r.getPoint();
-    //std::cout<<"ras"<<std::endl;
-    return true;
+    return (a*m>0 && a.norm()>d); //On détecte une intersection seulement si la sphère n'est pas derrière le pt de départ du rayon(a*m>0)
+                                  //et que le point de départ du rayon n'est pas dans la sphère (a.norm()>d)
 }
 
 void tracer(Camera ca, Scene s){
@@ -137,23 +136,23 @@ void tracer(Camera ca, Scene s){
 
 
 Color computeColor(Camera ca, Scene s, Ray origin, int index){//calcul la couleur du point vu "depuis" le vecteur origin
-    
+
     double dist = -1;//distance au point intersecté
     int j;
     Vector pos, res;//vecteur à intersecter
-    
+
     std::vector<Light> lights;
     //lights.push_back(Light(Vector(0,100,200), Color(1.,1.,1.),Color(0.,0.,0.)));
     lights.push_back(Light(Vector(0,-100,-200), Color(1.,1.,1.),Color(1.,1.,1.)));
     //recherc he une intersection
-    for(int i = 0; i < s.size(); i++){//pour chaque sphere        
+    for(int i = 0; i < s.size(); i++){//pour chaque sphere
         if(ray_sphere_intersect2(origin,s.getSphere(i),pos) && i != index){//si intersection, pas sur la même sphère
             if(dist == -1 || dist > (pos-origin.getVector()).norm()){//si intersection plus près
                 res = pos;//on change l'intersection
                 dist = (pos-origin.getVector()).norm();
                 j = i;
             }
-        }       
+        }
     }
     if(dist == -1){ //pas d'intersection'
         return  phongColor(ca, s, lights, s.getSphere(index), origin.getPoint());
@@ -163,8 +162,8 @@ Color computeColor(Camera ca, Scene s, Ray origin, int index){//calcul la couleu
         double r = s.getSphere(index).getR();
         return phongColor(ca, s, lights, s.getSphere(j), res).multiply(r).add(computeColor(ca,s,origin,j).multiply(1-r));
     }
-    
-    
+
+
 }
 
 void tracerRecursive(Camera ca, Scene s){
@@ -179,12 +178,12 @@ void tracerRecursive(Camera ca, Scene s){
     cimg_forXY(img,x,y) {  //for each pixel of the image
         Ray r = ca.Rayf(x,y);//rayon qui va de l'oeil au point (x.y)
         bool intersect = false;
-        
+
         double dist = -1;//distance au point intersecté
         int j = -1; //numero de la sphere finale retenue
         Vector pos, res;//vecteur à intersecter
-        
-        for(int i = 0; i < s.size(); i++){//pour chaque sphere            
+
+        for(int i = 0; i < s.size(); i++){//pour chaque sphere
             if(ray_sphere_intersect2(r,s.getSphere(i),pos)){//si intersection
                 intersect = true;
                 if(dist == -1 || dist > (pos-r.getPoint()).norm()){//si intersection plus près
@@ -193,21 +192,21 @@ void tracerRecursive(Camera ca, Scene s){
                     j = i;
                 }
             }
-        }  
-       
+        }
+
         if(intersect = true){//si intersection
             Color c = computeColor(ca, s, Ray(r.getVector(), pos), j);
             for(int i = 0; i <3; i++){
                 img(x,y,i) = c.getValue(i);
             }
         }
-        
+
         else{//sinon couleur de fond noire
             cout << "coucou" << endl;
             for(int i = 0; i < 3; i++){
                 img(x,y,i) = 0;
             }
-        }        
+        }
         //img(x,y,c) = pixel_value_at(x,y,c);
     }
     CImgDisplay main_disp(img,"Click a point");
